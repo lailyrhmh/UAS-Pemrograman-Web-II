c2f5c37c570d0a855c0677faca6c9c7f6f4684b9
lailyrhmh <lailyrhmh11@gmail.com>
2022-05-28 18:06:17 +0800

    edit tabel albums

--- database/migrations/2022_05_28_080933_create_albums_table.php
+++ database/migrations/2022_05_28_080933_create_albums_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('albums', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('artist');
             $table->timestamps();
         });
     }
