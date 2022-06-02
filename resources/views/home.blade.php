016f3574c50159208da787292ccb70ab1519aac8
lailyrhmh <lailyrhmh11@gmail.com>
2022-06-02 16:01:36 +0800

    partials view user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,47 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+@extends('layouts.main')
 
-    <title>GOODIES - Landing Page</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Animation CSS -->
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/style.css" rel="stylesheet">
-</head>
-<body id="page-top">
-<section  class="landing-page no-skin-config">
-    <div class="navbar-wrapper">
-            <nav class="navbar navbar-default navbar-fixed-top navbar-expand-md" role="navigation">
-                <div class="container">
-                    <a class="navbar-brand" href="#page-top">WEBAPPLAYERS</a>
-                    <div class="navbar-header page-scroll">
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
-                            <i class="fa fa-bars"></i>
-                        </button>
-                    </div>
-                    <div class="collapse navbar-collapse justify-content-end" id="navbar">
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a class="nav-link page-scroll" href="#page-top">Home</a></li>
-                            <li><a class="nav-link page-scroll" href="#albums">Albums</a></li>
-                            <li><a class="nav-link page-scroll" href="#contact">Contact</a></li>
-                            <li><a class="nav-link page-scroll" href="/login">LOGIN</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-    </div>
-    <div id="inSlider" class="carousel slide" data-ride="carousel" >
+@section('container')
+<div id="inSlider" class="carousel slide" data-ride="carousel" >
         <ol class="carousel-indicators">
             <li data-target="#inSlider" data-slide-to="0" class="active"></li>
             <li data-target="#inSlider" data-slide-to="1"></li>
@@ -481,75 +441,4 @@
             </div>
         </div>
     </section>
-</section>
-
-<!-- Mainly scripts -->
-<script src="js/jquery-3.1.1.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.js"></script>
-<script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
-<script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-
-<!-- Custom and plugin javascript -->
-<script src="js/inspinia.js"></script>
-<script src="js/plugins/pace/pace.min.js"></script>
-<script src="js/plugins/wow/wow.min.js"></script>
-
-
-<script>
-
-    $(document).ready(function () {
-
-        $('body').scrollspy({
-            target: '#navbar',
-            offset: 80
-        });
-
-        // Page scrolling feature
-        $('a.page-scroll').bind('click', function(event) {
-            var link = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $(link.attr('href')).offset().top - 50
-            }, 500);
-            event.preventDefault();
-            $("#navbar").collapse('hide');
-        });
-    });
-
-    var cbpAnimatedHeader = (function() {
-        var docElem = document.documentElement,
-                header = document.querySelector( '.navbar-default' ),
-                didScroll = false,
-                changeHeaderOn = 200;
-        function init() {
-            window.addEventListener( 'scroll', function( event ) {
-                if( !didScroll ) {
-                    didScroll = true;
-                    setTimeout( scrollPage, 250 );
-                }
-            }, false );
-        }
-        function scrollPage() {
-            var sy = scrollY();
-            if ( sy >= changeHeaderOn ) {
-                $(header).addClass('navbar-scroll')
-            }
-            else {
-                $(header).removeClass('navbar-scroll')
-            }
-            didScroll = false;
-        }
-        function scrollY() {
-            return window.pageYOffset || docElem.scrollTop;
-        }
-        init();
-
-    })();
-
-    // Activate WOW.js plugin for animation on scrol
-    new WOW().init();
-
-</script>
-
-</body>
-</html>
+@endsection
