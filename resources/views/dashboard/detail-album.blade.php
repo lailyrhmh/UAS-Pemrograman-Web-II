312681477141371e4a34c1e6335ba92755fb1933
neysanisrina <99717369+neysanisrina@users.noreply.github.com>
2022-06-03 19:27:40 +0800

    mengelompokan view admin

---
+++ resources/views/dashboard/detail-album.blade.php
@@ -0,0 +1,100 @@
+@extends('dashboard.main')
+
+@section('content')
+
+<div class="wrapper wrapper-content animated fadeInRight">
+
+    <div class="row">
+        <div class="col-lg-12">
+
+            <div class="ibox product-detail">
+                <div class="ibox-content">
+
+                    <div class="row">
+                        <div class="col-md-5">
+
+
+                            <div class="product-images">
+
+                                <div>
+                                    <div class="image-imitation">
+                                        [IMAGE 1]
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="image-imitation">
+                                        [IMAGE 2]
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="image-imitation">
+                                        [IMAGE 3]
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+                        </div>
+                        <div class="col-md-7">
+
+                            <h1 class="font-bold m-b-xs">
+                                ALBUM TITLE
+                            </h1>
+                            <h3>ARTIST NAME</h3>
+                            <hr>
+
+                            <h4>Album Description</h4>
+
+                            <div class="medium text-muted">
+                                It is a long established fact that a reader will be distracted by the readable
+                                content of a page when looking at its layout. The point of using Lorem Ipsum is
+
+                                <br/>
+                                <br/>
+                                There are many variations of passages of Lorem Ipsum available, but the majority
+                                have suffered alteration in some form, by injected humour, or randomised words
+                                which don't look even slightly believable.
+                            </div>
+                            <dl class="large m-t-md">
+                                <dt>Release Date</dt>
+                                <dd>dd/mm/yy</dd>
+                                <dt>Label</dt>
+                                <dd>Artist Agency</dd>
+                                <dt>Song List</dt>
+                                <dd>- 
+                                    -
+                                </dd>
+                                <dt>Version</dt>
+                                <dd>-
+                                    -
+                                </dd>
+
+                            </dl>
+                            <hr>
+
+                            <div>
+                                <div class="btn-group">
+                                    <a href="/albumform" class="btn btn-l btn-outline btn-primary">Edit</i> </a>
+                                    <a href="/" class="btn btn-l btn-outline btn-danger">Delete</a>   
+                                </div>
+                            </div>
+
+
+
+                        </div>
+                    </div>
+
+                </div>
+                <div class="ibox-footer">
+                    <span class="float-right">
+                        Full stock - <i class="fa fa-clock-o"></i> 14.04.2016 10:04 pm
+                    </span>
+                    The generated Lorem Ipsum is therefore always free
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/dashboard/list-talent.blade.php
@@ -0,0 +1,26 @@
+@extends('dashboard.main')
+
+@section('content')
+<div class="wrapper wrapper-content animated fadeInRight">
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="contact-box">
+                <a class="row" href="profile.html">
+                <div class="col-4">
+                    <div class="text-center">
+                        <img alt="image" class="rounded-circle m-t-xs img-fluid" src="img/a2.jpg">
+                    </div>
+                </div>
+                <div class="col-8">
+                    <h3><strong>ENHYPEN</strong></h3>
+                    <p><i class="fa fa-building"></i> BE:LIFT LAB</p>
+                    <address>
+                        <strong>Debut Date</strong><br>
+                        30 November 2020 <br>
+                    </address>
+                </div>
+                    </a>
+            </div>
+        </div>
+
+ @endsection
