658410706983e933adaabb2416a08f33a2c75614
lailyrhmh <lailyrhmh11@gmail.com>
2022-06-06 17:10:34 +0800

    image controller

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Image;
+use Illuminate\Http\Request;
+
+class ImageController extends Controller
+{
+    //
+
+    public function create()
+    {
+        // $talent = Talent::all();
+        // return view('dashboard.form-albumAdd', [
+        //     "title" => "Info Album"
+        // ], compact('talent'));
+        
+    }
+
+    public function store(Request $request)
+    {
+        // return $request->file('image')->store('album-image');
+        // dd($request->all());
+
+        $this->validate($request, [
+            'image' => 'required|file',
+        ]);
+
+        $image = Image::create([
+            'title' => $request->title,
+            'talent_id' => $request->talent_id,
+            'description' => $request->description,
+        ]);
+
+        // dd($request);
+
+        if ($image) {
+            return redirect()
+                ->route('album.index')
+                ->with([
+                    'success' => 'New Album has been created successfully'
+                ]);
+        } else {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with([
+                    'error' => 'Some problem occurred, please try again'
+                ]);
+        }
+    }
+}
