9eef72e202adedf7078913822b917c7d96cc105d
lailyrhmh <lailyrhmh11@gmail.com>
2022-06-06 08:31:28 +0800

    button cancel

--- resources/views/dashboard/form-albumAdd.blade.php
+++ resources/views/dashboard/form-albumAdd.blade.php
@@ -68,7 +68,7 @@
                         <div class="hr-line-dashed"></div>
                         <div class="form-group row">
                             <div class="col-sm-4 col-sm-offset-2">
-                                <button class="btn btn-white btn-sm" type="submit">Cancel</button>
+                                <a href="{{ route('album.index') }}" class="btn btn-white btn-sm" type="button">Cancel</a>
                                 <button class="btn btn-primary btn-sm" type="submit">Save changes</button>
                             </div>
                         </div>
